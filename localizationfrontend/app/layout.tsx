5cbfe164fd050a2b393eca45ad7355ba4e97444b
smorodya74 <positivepel@gmail.com>
2025-07-05 17:28:04 +0500

    frontend: buffer

--- localizationfrontend/app/layout.tsx
+++ localizationfrontend/app/layout.tsx
@@ -1,4 +1,8 @@
+'use client'
+import '@ant-design/v5-patch-for-react-19';
+import { Layout } from "antd";
 import "./globals.css";
+import { Footer } from "antd/es/layout/layout";
 
 export default function RootLayout({
   children,
@@ -7,8 +11,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body style={{ background: '#7566D1' }}>
         {children}
+        <Footer
+          style={{
+            borderTop: '1px solid #404040',
+            textAlign: "center",
+            background: 'transparent',
+            color: '#000000'
+          }}
+        >
+          Localization Service © 2025 Created by Stepan Smorodnikov
+        </Footer>
       </body>
     </html>
   );
--- localizationfrontend/app/page.tsx
+++ localizationfrontend/app/page.tsx
@@ -1,5 +1,19 @@
+'use client';
+import dynamic from 'next/dynamic';
+import type { TranslationTableProps } from '@/app/Components/Table';
+import { Color } from 'antd/es/color-picker';
+import { AlignCenterOutlined } from '@ant-design/icons';
+
+const TranslationTable = dynamic<TranslationTableProps>(
+  () => import('@/app/Components/Table').then(m => m.default),
+  { ssr: false }
+);
+
 export default function Home() {
   return (
-    <p>HOME PAGE</p>
+    <>
+      <p>HOME PAGE</p>
+      <TranslationTable pageSize={10} />
+    </>
   );
 }
